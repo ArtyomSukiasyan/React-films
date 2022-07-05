e0be1018768d7d5568814ca290a8e8ff40ab2d90
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-07-05 13:07:06 +0400

    feat: add Image component

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,14 @@
+import "./Image.scss"
+
+export default function Image({ backdrop_path, original_title }: any) {
+  return (
+    <img
+      src={
+        backdrop_path
+          ? "https://image.tmdb.org/t/p/w500/" + backdrop_path
+          : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
+      }
+      alt={original_title}
+    />
+  );
+}
