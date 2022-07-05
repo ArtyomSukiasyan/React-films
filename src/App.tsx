0ca4d34938203092c5dc62cfed2b8c27d1746306
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-07-05 14:36:03 +0400

    feat: add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,35 @@ import { ReactElement } from "react";
 import Header from "./components/Header/Header";
 import Films from "./components/Films/Films";
 import "./App.scss";
+import { Navigate, Route, Routes } from "react-router-dom";
+import SignIn from "./components/SignIn/SignIn";
+import Login from "./components/LogIn/Login";
+import ErrorPage from "./components/404/404";
 
 function App(): ReactElement {
+  const currentUser = localStorage.getItem("currentUser") || "";
+
   return (
     <>
       <Header />
-      <Films />
+      <div className="content-wrapper">
+        <Routes>
+          <Route path="/" element={<Films />}></Route>
+
+          {currentUser ? (
+            <Route path="/favourites" element={<Films />}></Route>
+          ) : (
+            <Route
+              path="/favourites"
+              element={<Navigate to="/login" replace />}
+            />
+          )}
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Routes>
+      </div>
     </>
   );
 }
