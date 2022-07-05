5d2815df70a81a503edc6d2d9ebdbb860b8b8299
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-07-05 14:35:13 +0400

    feat: add Validation Messages and Regex

---
+++ src/constants/Regex.ts
@@ -0,0 +1,9 @@
+export const nameCheckRegex = /^[a-zA-Z ]{1,30}$/;
+
+export const surnameCheckRegex = /^[a-zA-Z ]{1,30}$/;
+
+export const emailCheckRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const passwordCheckRegex =
+  /^(?=.*[0-9])(?=.*[!@#$%^&*?])[a-zA-Z0-9!@#$%^&*?]{6,16}$/;
---
+++ src/constants/ValidationMessages.ts
@@ -0,0 +1,15 @@
+export const isInvalidNameMessage = "Please, enter a valid name";
+
+export const isInvalidSurnameMessage = "Please, enter a valid surname";
+
+export const isInvalidEmailMessage = "Please, enter a valid email";
+
+export const isInvalidPasswordMessage = "Please, enter a valid password";
+
+export const alreadyRegistredMessage = "You're registered, please login";
+
+export const notFoundEmailMessage = "Email not found, please register";
+
+export const isWrongPasswordMessage = "Wrong password!";
+
+export const emptyString = ""
